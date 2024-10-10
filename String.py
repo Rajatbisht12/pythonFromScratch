69dc130e0ae6637814810c425f4544e45396dbe3
Rajatbisht12 <91795456+Rajatbisht12@users.noreply.github.com>
2024-10-10 20:07:29 +0000

    Strings

---
+++ String.py
@@ -0,0 +1,15 @@
+str = "Hello, World!"
+str1 = "this is a string\nthe second line"
+# --------------------------Conacatenate strings------------------------------------
+# str2 = str + str1
+length = len(str)
+# -----------------------------Indexing---------------------------------------------
+str2 = "HEllo, World!"
+# -----------------------------Slicing----------------------------------------------
+str3 = str2[0:5]
+
+# ------------------------------FUNCTIONS-------------------------------------------
+val = str2.endswith("rld!")
+str2 = str2.capitalize() #Capitalize first letter of the string
+
+name = input("Enter your name: ")
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 # from function import *
-
 # # name = "Rajat"
 # # age = 22
 # # print(f"Hello, My name is {name} and I'm {age} years old.")
-
-
 # name = input("Enter your name: ")
 # # age = int(input("Enter your age: "))
 # # print(f"Hello, My name is {name} and I'm {age} years old.")
-
 # a = int(input("Enter first number: "))
 # b = int(input("Enter second number: "))
-
 # print(f"The sum of {a} and {b} is: {sum(a, b)}")
 # print(f"The greeting is: {greet(name)}")
+
+# --------------------------------program 2 ------------------------------------------
+from String import *
+# print(length)
+# print(str3)
+# for i in range(0,len(str2)):
+#     print(str2[i]+"\n")
+# print(val)
+# print(str2)
+# print(len(name))
+# ----------------------------------program 3-----------------------------------------
+
