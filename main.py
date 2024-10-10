a460bae810e19b4dfe98367a73d3c1196ce175d7
Rajatbisht12 <91795456+Rajatbisht12@users.noreply.github.com>
2024-10-10 07:58:14 +0000

    changes

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-name = "Rajat"
-age = 22
-print(f"Hello, My name is {name} and I'm {age} years old.")
+# from function import *
+
+# # name = "Rajat"
+# # age = 22
+# # print(f"Hello, My name is {name} and I'm {age} years old.")
+
+
+# name = input("Enter your name: ")
+# # age = int(input("Enter your age: "))
+# # print(f"Hello, My name is {name} and I'm {age} years old.")
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# print(f"The sum of {a} and {b} is: {sum(a, b)}")
+# print(f"The greeting is: {greet(name)}")
